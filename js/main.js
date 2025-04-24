065bf5b69864e57b822c5e8c774b7dc152b671f5
Rustam <189756814+rustleium@users.noreply.github.com>
2025-04-24 20:00:05 +0530

    feat: add open and close logic for about window

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 const aboutIcon = document.getElementById('aboutIcon');
 const aboutWin = document.getElementById('aboutWindow');
 const closeWin = document.querySelector('.closeBtn');
+
+aboutIcon.addEventListener('click', () => {
+  aboutWin.style.display = "block";
+})
+
+closeWin.addEventListener('click', () => {
+  aboutWin.style.display = "none";
+})
